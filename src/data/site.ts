e87c74208fadfe3890ca17ced499eeaafec09328
lemiffe <mike.war@gmail.com>
2026-06-18 22:28:41 +0200

    Minor copy + design updates

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,7 +1,6 @@
 export const site = {
     name: "Baaarry",
     email: "baaarry@lemiffe.com",
-    // Placeholder until the real Gumroad URL is finalised.
     downloadUrl: "https://lemiffe.gumroad.com/l/baaarry",
     copyright: "© 2026 Lemiffe",
 } as const;
